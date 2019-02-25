d41ea4959faf0a5b8a38fa13c649902401f1bfeb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-25 16:41:14 -0500

    Added 'key' prop to <Link /> components in .map() function of MovieList.js file

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -25,7 +25,7 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
             <MovieDetails key={movie.id} movie={movie} />
           </Link>
         ))}
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -9,8 +9,8 @@ export default class SavedList extends Component {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+        {this.props.list.map((movie, index) => (
+          <span key={index} className="saved-movie">{movie.title}</span>
         ))}
         <div className="home-button">Home</div>
       </div>

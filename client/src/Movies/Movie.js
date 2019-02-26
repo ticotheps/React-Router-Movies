b0b65eac36af079d91e25e39967a484ed70719ca
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-26 17:30:55 -0500

    Refactored Movie.js file to produce DRY code

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 import axios from 'axios';
 
 export default class Movie extends Component {
@@ -42,24 +43,14 @@ export default class Movie extends Component {
     }
 
     const { title, director, metascore, stars } = this.state.movie;
+    // This is the same thing as:
+    //    const title = this.state.movie.title;
+    //    const director = this.state.movie.director;
+    //    const metascore = this.state.movie.metascore;
+    //    const stars = this.state.movie.stars;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+        <MovieCard movie={this.state.movie} />
         <div className="save-button">Save</div>
       </div>
     );

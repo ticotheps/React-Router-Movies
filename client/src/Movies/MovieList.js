42ca93777341418e83b6ef6fa2a11e78df29c93b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-02-26 17:15:29 -0500

    Refactored MovieList.js

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 import axios from 'axios';
 
 export default class MovieList extends Component {
@@ -26,32 +27,10 @@ export default class MovieList extends Component {
       <div className="movie-list">
         {this.state.movies.map(movie => (
           <Link key={movie.id} to={`/movies/${movie.id}`}>
-            <MovieDetails movie={movie} />
+            <MovieCard movie={movie} />
           </Link>
         ))}
       </div>
     );
   }
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
 }
